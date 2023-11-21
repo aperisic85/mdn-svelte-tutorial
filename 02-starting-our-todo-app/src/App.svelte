f5b3926ae07194ca1dc88ab64832ed592601b087
Ante Perisic <perisic.ante@gmail.com>
2023-11-21 23:14:28 +0100

    -end 2

--- 02-starting-our-todo-app/src/App.svelte
+++ 02-starting-our-todo-app/src/App.svelte
@@ -1,19 +1,9 @@
 <script>
-  export let name = 'world';
-
-  function toggleName() {
-    if (name === 'world') {
-      name = 'svelte'
-    } else {
-      name = 'world'
-    }
-  }
+  import Todos from "./components/Todos.svelte"
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <button on:click={toggleName}>Toggle name</button>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<Todos></Todos>
 </main>
 
 <style>
